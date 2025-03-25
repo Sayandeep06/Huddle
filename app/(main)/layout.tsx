c6d0f8694a3b10b2c724fe4459016cbe096b37a7
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-03-25 19:23:58 +0530

    landing page

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -14,29 +14,23 @@ const MainLayout = async ({
 
     const user = await currentUser()
     if(!user)  return(
-            <main className="flex flex-col items-center p-5 gap-10 animate-fade-in">
-    
-                        <section className="flex flex-col items-center">
-                            <Image
-                                src='/assets/logo.svg'
-                                width={100}
-                                height={100}
-                                alt="Logo"
-                                />
-                                <h1 className="text-lg font-extrabold text-sky-1 lg:text-2xl">
-                                Connect, Communicate, Collaborate in Real-Time
-                                </h1>
-                         
-                       </section>
-    
-                <div className="mt-3">
-                    <SignIn
-                    routing="hash"
-                        appearance={{
-                                baseTheme: neobrutalism
-                            }
-                        }
-                    />
+            <main className="flex flex-col md:flex-row w-full min-h-screen bg-gradient-to-r from-blue-600 to-purple-600">
+                <section className="flex flex-col justify-center items-center md:items-start mt-6 mb-7 md:mb-0 md:mt-0 flex-1 px-8 md:px-16 text-white text-center md:text-left">
+                    <div className="flex items-center gap-2">
+                        <h1 className="text-4xl md:text-5xl font-bold">HuddleX</h1>
+                        <Image src="/assets/zoom.png" width={60} height={60} alt="Logo" className="self-center" />
+                    </div>
+                    <h1 className="text-2xl md:text-5xl font-bold mt-4">
+                        Welcome Back!<br />
+                        <span className="text-yellow-300">Ready to Huddle Again?</span>
+                    </h1>
+                    <p className="text-md mt-3 text-gray-200">
+                        Sign In To Create A New Room Or Join A Room
+                    </p>
+                </section>
+
+                <div className="flex justify-center items-center flex-1 px-8 mb-15 md:mb-0 md:px-16">
+                    <SignIn />
                 </div>
             </main>
         )
